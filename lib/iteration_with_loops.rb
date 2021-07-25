f44ead9ee4b2b58e65cb68ae2f4b735baf0ca10a
tuanlong1202 <tuanlong1202@gmail.com>
2021-07-25 12:23:25 -0400

    Phase 3 nested arrays iteration lab part 3

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -1,4 +1,17 @@
 def join_nested_strings(src)
   # src will be an Array of Arrays of Strings and Integers
   # Combine all Strings present in the AoA into a single value and return it
+  retArray = []
+  rowIndex = 0
+  while rowIndex < src.count do
+    columnIndex = 0
+    while columnIndex < src[rowIndex].count do
+      if src[rowIndex][columnIndex] .is_a?(String)
+        retArray << src[rowIndex][columnIndex]
+      end
+      columnIndex += 1
+    end
+    rowIndex += 1
+  end
+  retArray.join(" ")
 end
